4927a88ab2cbbf9e5a31c9d68b647b27121676c6
Keith Ssewanonda <38857594+edcsu@users.noreply.github.com>
2021-09-13 19:14:36 +0300

    feat: add models and schema

--- server.js
+++ server.js
@@ -3,6 +3,39 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+const mongoose = require('mongoose');
+const mongoURI = process.env['MONGO_URI']
+const { Schema } = mongoose;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Create exercise Schema
+const exerciseSchema = new Schema({
+  username: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: String, required: true }
+})
+const Exercise = mongoose.model("exercise", exerciseSchema);
+
+
+// Create user Schema
+const userSchema = new Schema({
+  username: { type: String, required: true },
+})
+const User = mongoose.model("user", userSchema);
+
+// Create log Schema
+const logSchema = new Schema({
+  username: { type: String, required: true },
+  count: { type: Number, required: true },
+  log: [{ description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: String, required: true }, 
+  }],
+})
+const Log = mongoose.model("log", logSchema);
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
