88305f0d801a496de3a46df33c38f08531e726a5
Keith Ssewanonda <38857594+edcsu@users.noreply.github.com>
2021-09-14 04:17:50 +0300

    refactor: models and  schema

--- server.js
+++ server.js
@@ -22,13 +22,7 @@ const Exercise = mongoose.model("exercise", exerciseSchema);
 // Create user Schema
 const userSchema = new Schema({
   username: { type: String, required: true },
-});
-const User = mongoose.model("user", userSchema);
-
-// Create log Schema
-const logSchema = new Schema({
-  username: { type: String, required: true },
-  count: { type: Number, required: true },
+  count: { type: Number },
   log: [
     {
       description: { type: String, required: true },
@@ -37,7 +31,7 @@ const logSchema = new Schema({
     },
   ],
 });
-const Log = mongoose.model("log", logSchema);
+const User = mongoose.model("user", userSchema);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -97,31 +91,39 @@ app.post("/api/users/:_id/exercises", async (req, res) => {
   const id = req.params._id;
   const description = req.body.description;
   const duration = req.body.duration;
-  const date = req.body.date;
+  const date = !req.body.date ? Date.now() : req.body.date;
 
   try {
     // check if user exists
-    let foundUser = await User.findById(id);
-    if (!foundUser) {
-      return res.json({ error: `User not found with ${id}.`});
-    } else {
-      // create new exercise record
-      let exercise = new Exercise({
-        username : foundUser.username,
-        description,
-        duration,
-        date: new Date(date).toDateString()
-      });
-      const savedExercise = await exercise.save();
-      console.log(savedExercise)
-      return res.json({
-        username: savedExercise.username,
-        description: savedExercise.description,
-        duration: savedExercise.duration,
-        date: savedExercise.date,
-        _id: savedExercise._id
-      });
-    }
+    await User.findById(id).exec((error, data) => {
+      if(error) console.log(error);
+      
+      else if (data === null) {
+        return res.json({ error: `User not found with ${id}.`});
+      } else {
+        // create new log
+        let newLog = {
+          username : data.username,
+          description,
+          duration,
+          date: new Date(date).toDateString()
+        };
+        data.log = data.log.concat(newLog);
+        data.log = data.log.sort((a, b) => a.date - b.date);
+        
+        data.save((error) => {
+          if (error) return console.error(error)
+        });
+
+        return res.json({
+          username: data.username,
+          count: data.log.length,
+          _id: data._id,
+          log: data.log,
+        });
+      }
+    });
+
   } catch (err) {
     console.error(err);
     return res.status(500).json("oops something broke");
@@ -131,7 +133,9 @@ app.post("/api/users/:_id/exercises", async (req, res) => {
 // get logs
 app.get("/api/users/:_id/logs", async (req, res) => {
   const id = req.params._id;
-  const limit = req.query.limit;
+  let limit = req.query.limit;
+  let from = req.query.from;
+  let to = req.query.to;
 
   try {
     // check if user exists
@@ -140,15 +144,24 @@ app.get("/api/users/:_id/logs", async (req, res) => {
       return res.json({ error: `User not found with ${id}.`});
     } else {
       // get user logs
-      const logs = await Log.findById(id)
-      .sort({ date: -1 })
+      if (from === undefined) {
+        from = new Date(0);
+      }
+      if (to === undefined) {
+          to = new Date();
+      }
+      if (limit === undefined) {
+          limit = 0;
+      } else {
+          limit = parseInt(limit);
+      }
+
+      await Exercise.find({username:foundUser.username, date: {$lt: to, $gt: from}})
       .limit(limit)
-      .select({ age: 0 })
-      .exec()
-      
-      return res.json({
-        ...logs
-      });
+      .exec((error, exercises) => {
+        if(error) console.log(error);
+        res.json(exercises);
+      })
     }
   } catch (err) {
     console.error(err);
